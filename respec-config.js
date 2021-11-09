61080ea32e854428546b4993c3c44863acbb8b49
Michael Cooper <cooper@w3.org>
2021-11-09 11:28:23 -0500

    Config as editors draft for new go-around

--- respec-config.js
+++ respec-config.js
@@ -21,18 +21,18 @@ var respecConfig = {
 	
 	// if you wish the publication date to be other than today, set this
 	//publishDate:  "2014-12-11",
-	copyrightStart:  "2018",
+	copyrightStart:  "2021",
 	license: "w3c-software-doc",
 	
 	// if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
 	// and its maturity status
-	previousPublishDate:  "2005-11-23",
+	previousPublishDate:  "2019-12-09",
 	previousMaturity:  "NOTE",
 	//prevRecURI: "",
 	//previousDiffURI: "",
 	
 	// if there a publicly available Editors Draft, this is the link
-	edDraftURI: "https://w3c.github.io/apa/captcha/",
+	edDraftURI: "https://w3c.github.io/captcha-accessibility/",
 	
 	// if this is a LCWD, uncomment and set the end of its review period
 	// lcEnd: "2012-02-21",
@@ -48,19 +48,16 @@ var respecConfig = {
 		{
 			name: "Janina Sajka",
 			url: "http://rednote.net/",
-			mailto: "janina@rednote.net",
 			w3cid: 33688
 		},
 		{
 			name: "Jason White",
 			url: "http://www.ets.org/",
-			mailto: "jjwhite@ets.org",
 			w3cid: 74028
 		},
 		{
 			name: "Michael Cooper",
 			url: 'https://www.w3.org',
-			mailto: "cooper@w3.org",
 			company: "W3C",
 			companyURI: "https://www.w3.org",
 			w3cid: 34017
@@ -95,20 +92,7 @@ var respecConfig = {
 	// errata: 'https://www.w3.org/2010/02/rdfa/errata.html',
 	
 	// name of the WG
-	wg:           "Accessible Platform Architectures Working Group",
-	
-	// URI of the public WG page
-	wgURI:        "https://www.w3.org/WAI/APA/",
-	
-	// name (with the @w3c.org) of the public mailing to which comments are due
-	wgPublicList: "public-apa",
-	
-	// URI of the patent status for this WG, for Rec-track documents
-	// !!!! IMPORTANT !!!!
-	// This is important for Rec-track documents, do not copy a patent URI from a random
-	// document unless you know what you're doing. If in doubt ask your friendly neighbourhood
-	// Team Contact.
-	wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/83907/status",
+	group:           "apa",
 	maxTocLevel: 4,
 	
 };
